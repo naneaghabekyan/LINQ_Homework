cb0d2fbd60ad7e7709d6a9464d740511d5f82d55
naneaghabekyan <naneaghabekyan201@gmail.com>
2026-05-18 12:21:09 +0400

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// 1․ Հաշվել միջին գնահատականը
+/* 2․ (Բոնուս) Գրիր ծրագիր, որը GroupBy-ի միջոցով ուսանողներին խմբավորում է ըստ գնահատականի և տպում․
+      Grade 90: 2 students
+      Grade 75: 1 student
+*/
 namespace LINQ_Homework
 {
     public class Student
@@ -17,4 +23,34 @@ namespace LINQ_Homework
             Grade=grade;
         }
     }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Student> students = new List<Student>
+            {
+                new Student("Jack",98),
+                new Student("Aren",98),
+                new Student("Lucy",76),
+                new Student("Anna",89),
+                new Student("Alena",98),
+                new Student("Eva", 67.5),
+                new Student("Lucy",76),
+            };
+
+            double avgGrade = students.Average(i => i.Grade);
+            Console.WriteLine($"Average grade:{avgGrade}");
+   
+            var groupStudents = students.GroupBy(i => i.Grade);
+            foreach (var student in groupStudents)
+            {
+                Console.WriteLine($"Grade {student.Key}: {student.Count()} students");
+            }
+
+        }
+    }
+
 }
